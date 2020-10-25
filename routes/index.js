64e5e07240e30e5457b99fb0a84c9c6b19e0e05a
Shouraya <shourayagoyal2406@gmail.com>
2020-10-25 20:33:31 +0530

    fixed the bug on sign up page

--- routes/index.js
+++ routes/index.js
@@ -22,8 +22,8 @@ router.post("/register", function(req, res){
     var newUser = new User({username: req.body.username});
     User.register(newUser, req.body.password, function(err, user){
         if(err) {
-            req.flash("error", err.message);
-            return res.render("register");
+            // req.flash("error", err.message);
+            return res.render("register", {"error": err.message});
         } 
         passport.authenticate("local")(req, res, function(){
             req.flash("success", "Welcome to Hotel Simplified " + user.username);
