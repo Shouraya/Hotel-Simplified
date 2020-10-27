1dc390f5b015f32a5dcb15cbb2e1b421c031a756
Shouraya <shourayagoyal2406@gmail.com>
2020-10-27 14:50:30 +0530

    deploy 2

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({path: ".env"});
 const express = require ("express"),
       app = express(),
       bodyParser = require("body-parser"), //To get data from form
