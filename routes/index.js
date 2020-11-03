46fe29173f9829205397255d3de4ea7b0d09e3cb
Shouraya <shourayagoyal2406@gmail.com>
2020-11-03 18:08:43 +0530

    changed navbar and fixed spelling in mail

--- routes/index.js
+++ routes/index.js
@@ -172,7 +172,7 @@ router.post('/reset/:token', function(req, res){
       var mailOptions = {
         to: user.email,
         from: 'shourayagoyal2000@gmail.com',
-        subject: 'Password Rest Successful !',
+        subject: 'Password Reset Successful !',
         text: 'Hello,\n\n' +
           'This is a confirmation that the password for your account ' + user.email + ' has just been changed.\n'
       };
